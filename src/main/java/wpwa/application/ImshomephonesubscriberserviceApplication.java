63656d5eb91fce64df151d8db72389d31710e352
fecalderon <felino.calderon@telusinternational.com>
2022-03-12 09:44:32 -0500

    Small update - removed unnecessary imports

--- src/main/java/wpwa/application/ImshomephonesubscriberserviceApplication.java
+++ src/main/java/wpwa/application/ImshomephonesubscriberserviceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import wpwa.application.exception.ResourceNotFoundException;
 import wpwa.application.model.Subscriber;
 import wpwa.application.repository.SubscriberRepository;
 
